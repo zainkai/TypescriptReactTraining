bc6a43d1e44d95086b0231db6bc3cb213e8e90a7
Struth <struthj@oregonstate.edu>
2017-05-04 16:51:49 -0700

    Revert "Typescript training app"
    This reverts commit 05aeb3ac095139f5d8bfe841e231f9168fff6570.

--- TypeScriptHTMLApp1/app.tsx
+++ TypeScriptHTMLApp1/app.tsx
@@ -1,13 +1,9 @@
-﻿window.onload = () => {
+﻿
+window.onload = () => {
     const el = document.getElementById('content');
     const jsx = <ToDoList name="CASS Student" />;
     ReactDOM.render(jsx, el);
 };
-
-interface Item {
-    name: string;
-    completed: boolean;
-}
 
 
 interface Props {
@@ -15,69 +11,44 @@ interface Props {
 }
 
 interface State {
-    items: Item[];
+    items: string[];
     newItemName: string;
-    newCompleted: boolean;
 }
 
 class ToDoList extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        var newItem: Item = { name: "Apple", completed: true };
-        this.state = {
-            items: [newItem],
-            newItemName: "",
-            newCompleted: false
-        };
-    }
 
-    onChecked = (e: React.ChangeEvent<HTMLInputElement>) => {
-
-        const newState = {
-            items: this.state.items,
-            newItemName: this.state.newItemName,
-            newCompleted: !this.state.newCompleted
-        };
-        this.setState(newState);
+    state = {
+        items: ["Milk", "Eggs", "Bread"],
+        newItemName: "",
+        butt: 42
     }
 
     onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const newState = {
-            items: this.state.items,
+            ...this.state,
             newItemName: e.currentTarget.value
         };
         this.setState(newState);
     };
 
     addItem = () => {
-        var newItem: Item = { name: this.state.newItemName, completed: this.state.newCompleted };
-        const newItems = [...this.state.items, newItem];
+        const newItems = [...this.state.items, this.state.newItemName];
         const newState = {
             items: newItems,
-            newItemName: "",
-            newCompleted: false
+            newItemName: ""
         };
         this.setState(newState);
     };
 
-    renderCheckBox(prop: Item) {
-       var check = "";
-       if (prop.completed) {
-           check = "✓";
-       }
-        return <li> {prop.name} {check}</li>;
-    };
-
     render() {
+        const itemsJSX = this.state.items.map(item => <li>{item}</li>);
 
-        const itemsJSX = this.state.items.map(item =>  this.renderCheckBox(item) );
         return (
             <div>
                 <h3>{this.props.name}'s To-Do List</h3>
                 <ul>
                     {itemsJSX}
                 </ul>
-                <input type="checkbox" id="completed" name="Completed" checked={this.state.newCompleted} onChange={this.onChecked} />
                 <input value={this.state.newItemName} onChange={this.onInputChange} />
                 <button onClick={this.addItem}>Add</button>
             </div>
